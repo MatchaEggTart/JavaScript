f2c333fc06699e9b0bb593cd601e3717e30525da
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-11-12 23:39:25 +0800

    update

---
+++ 01_TypeScript/03_Data_Type/Data_Type.ts
@@ -0,0 +1,19 @@
+console.log("Number Typs");
+console.log("\n");
+let a: number;
+let b: number = 2;
+a = NaN;
+console.log("Value:\n");
+console.log("a = " + a + "\nb = " + b);
+console.log("\n");
+console.log("Typeof:\n");
+console.log("typeof(a) = " + typeof (a) + "\ntypeof(b) = " + typeof (b));
+console.log("\n");
+console.log("a + 0 = " + (a + 0));
+console.log("a ** 0 = " + a ** 0);
+console.log("\n");
+
+console.log("BigInt Types");
+console.log("\n");
+const c = BigInt(Number.MAX_SAFE_INTEGER)
+
